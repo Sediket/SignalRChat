8eddd05537ea611ff7826126252bd3a5f96d452a
mbazzazi1 <miladbazzazi@mail.weber.edu>
2020-02-25 19:36:06 -0700

    Add files via upload

---
+++ Model/User.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XXXX
+{
+    public class Users
+    {
+        public int userID { get; set; }
+        public string userName { get; set; }
+        public string userColor { get; set; }//or rgb?
+    }
+}
